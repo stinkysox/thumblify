397c82970387b773c6caac566dbad033ec082b50
Hevit <linkinparknumb303@gmail.com>
2026-01-10 13:09:27 +0530

    updated-cookies

--- server/server.ts
+++ server/server.ts
@@ -49,8 +49,10 @@ app.use(
     cookie: {
       maxAge: 1000 * 60 * 60 * 24 * 7,
       // For Vercel cross-domain to work, these two lines are the "secret sauce"
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
       sameSite: "none",
-      secure: true,
+      path: "/",
     },
   })
 );
